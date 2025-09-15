019c246cd7f7bb82980c2e05a76500c30be67306
Leon Bojanowski <leongaborbojanowski04@gmail.com>
2025-09-15 15:18:52 +0200

    init commit

---
+++ src/client/java/f1nal/bringbackdisarmedtripwire/BringBackDisarmedTripwireClient.java
@@ -0,0 +1,10 @@
+package f1nal.bringbackdisarmedtripwire;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BringBackDisarmedTripwireClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
